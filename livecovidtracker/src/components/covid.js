19142ef2f6d9d6228645ec3a9b6a4d5c84349b75
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-01-24 22:32:46 +0530

    added comments to make it easy to understand

--- livecovidtracker/src/components/covid.js
+++ livecovidtracker/src/components/covid.js
@@ -3,11 +3,15 @@ import React, { useEffect } from "react"
 
 const App = () => {
   //getting the data
+  //await is used to wait for a promise
+  //async makes sure that a promise is surely returned by the api
   const getCovidData = async () => {
     const res = await fetch("https://data.covid19india.org/data.json")
     const actualData = await res.json()
     console.log(actualData.statewise[0])
   }
+
+  //useEffect is used to update the data whenerver the page is refreshed for the first time
   useEffect(() => {
     getCovidData()
   }, [])
