b2bf359b9b0ba1c3c2bc2f8f3f60e00baf8fdd01
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-01-29 20:08:19 +0530

    created component for cards

--- understanding-props/src/index.js
+++ understanding-props/src/index.js
@@ -3,24 +3,7 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import React from "react"
 import ReactDOM from "react-dom"
-
-function Card(props) {
-  return (
-    <div className="cards">
-      <div className="card">
-        <img src={props.imgsrc} alt="myPic" className="card__img" />
-        <div className="card__info">
-          <span className="card_category">{props.title}</span>
-          <h3 className="card__title">{props.sname} </h3>
-          <a href={props.link} target="_blank" rel="noreferrer">
-            <button>Watch Now</button>
-          </a>
-        </div>
-      </div>
-    </div>
-  )
-}
-
+import Card from "./Cards"
 ReactDOM.render(
   <>
     <Card
